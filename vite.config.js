f7872f324775234abfb1c247acdba8c907fb111f
Albert <nonono28693346@gmail.com>
2025-04-12 00:27:53 +0800

    我的本地修改

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: 'https://github.com/albertfan317/newkiller_web/', // 這裡應該使用你的GitHub倉庫名稱
+  base: '/newkiller_web/', // 這裡應該使用你的GitHub倉庫名稱
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
