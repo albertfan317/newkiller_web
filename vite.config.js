967d859fa41c2f96b45241de1567af643c342fa5
Albert_Fan <33210575+albertfan317@users.noreply.github.com>
2025-04-12 00:18:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -10,6 +9,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: 'https://github.com/albertfan317/newkiller_web/', // 這裡應該使用你的GitHub倉庫名稱
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
