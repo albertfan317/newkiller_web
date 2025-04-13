55c48a254a500ecd1fd940ac6335766a6d5aad24
Albert <nonono28693346@gmail.com>
2025-04-13 17:12:27 +0800

    初始提交

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  // 注意這裡的修改 - 使用import.meta.env.BASE_URL作為基礎路徑
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
